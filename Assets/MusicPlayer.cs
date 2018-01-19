145b0f7dfde53ff045ea7772d595b26cf2d5c2ed
Arracias <arracias@github.com>
2018-01-19 23:22:28 +0300

    Update
    Added splash screen and background music

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+     void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Invoke("LoadFirstScene", 3.0f);
+	}
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
