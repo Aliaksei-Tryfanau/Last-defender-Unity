e09408889a8ac8ee25fde98ca5ff8acb34b4face
Arracias <arracias@github.com>
2018-02-04 17:13:56 +0300

    Added UI score calculation

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 12;
+
+    ScoreBoard scoreBoard;
+
+	void Start () {
+        AddNonTriggerBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+	}
+
+    private void AddNonTriggerBoxCollider()
+    {
+        AddBoxCollider();
+    }
+
+    private void AddBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        scoreBoard.ScoreHit(scorePerHit);
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour {
+
+
+    int score;
+    Text scoreText;
+        
+	void Start () {
+        scoreText = GetComponent<Text>();
+	}
+
+    private void Update()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    public void ScoreHit(int scorePerHit)
+    {
+        score += scorePerHit;
+    }
+}
